018a319d7cb565a23f17c3e96079042d6b3caa64
Luis Gustavo Almeida Reis <luisgustavoareis@gmail.com>
2023-02-10 15:26:19 -0300

    Update TSB_LR_mapreduce.js

--- TSB_LR_mapreduce.js
+++ TSB_LR_mapreduce.js
@@ -15,7 +15,7 @@ define([
       /////////ESCOLHE AS INFORMAÇÕES QUE SERÃO PROCESSADAS 10.000gov pts
       function getInputData() {
 
-         var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+         var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
          log.debug({
             title: 'function getInputData',
